eaeba7ec079e17aa507f511e15cbbc23a092918b
georgedimmitrov <georgedimmitrov@gmail.com>
2018-02-27 23:37:15 +0200

    add UI angular code for app

--- simple-social-network/routes/app.js
+++ simple-social-network/routes/app.js
@@ -1,29 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const User = require('../models/user');
 
-router.get('/', async (req, res, next) => {
-  User.findOne({})
-    .then((doc) => {
-      res.render('node', { email: doc.email });
-    })
-    .catch((e) => {
-      console.log('Error!: ', e);
-    });
-});
-
-router.post('/', (req, res, next) => {
-  const email = req.body.email;
-  const user = new User({
-    firstName: 'Max',
-    lastName: 'Schwarz',
-    password: 'secret',
-    email
-  });
-
-  user.save();
-
-  res.redirect(`/`);
+router.get('/', (req, res, next) => {
+    res.render('index');
 });
 
 module.exports = router;
