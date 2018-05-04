d07c7f0cec94e419882c0d5d25d8bb24a44aadbe
georgedimmitrov <georgedimmitrov@gmail.com>
2018-05-04 23:00:24 +0300

    added two forms - one template driven and one reactive

--- recipe-shopping-list-app/src/app/shopping-list/shopping-list.service.ts
+++ recipe-shopping-list-app/src/app/shopping-list/shopping-list.service.ts
@@ -5,6 +5,7 @@ import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListService {
   // used to manually emit that ingredients have changed
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -15,10 +16,24 @@ export class ShoppingListService {
     return this.ingredients.slice();
   }
 
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
 
     // manually emit that ingredients have changed
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
