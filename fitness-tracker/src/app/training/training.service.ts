3bf2a1cb15dbbc53e679279be6244eb275fb581f
georgedimmitrov <georgedimmitrov@gmail.com>
2018-06-09 16:48:47 +0300

    added firebase dynamic loading

--- fitness-tracker/src/app/training/training.service.ts
+++ fitness-tracker/src/app/training/training.service.ts
@@ -1,20 +1,41 @@
+import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/internal/Subject';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { map } from 'rxjs/operators';
 
 import { Exercise } from './exercise.model';
 
+@Injectable()
 export class TrainingService {
   exerciseChanged = new Subject<Exercise>();
-  private availableExercises: Exercise[] = [
-    {id: 'crunches', name: 'Crunches', duration: 30, calories: 8},
-    {id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15},
-    {id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18},
-    {id: 'burpees', name: 'Burpees', duration: 60, calories: 8}
-  ];
+  exercisesChanged = new Subject<Exercise[]>();
+  private availableExercises: Exercise[] = [];
   private runningExercise: Exercise;
   private exercises: Exercise[] = [];
 
-  getAvailableExercises() {
-    return this.availableExercises.slice();
+  constructor(private db: AngularFirestore) {}
+
+  fetchAvailableExercises() {
+    this.db.collection('availableExercises')
+      .snapshotChanges()
+      .pipe(
+        map(docArray => {
+          return docArray.map(doc => {
+            const data = doc.payload.doc.data();
+
+            return {
+              id: doc.payload.doc.id,
+              name: data.name,
+              duration: data.duration,
+              calories: data.calories
+            };
+          });
+        })
+      )
+      .subscribe((exercises: Exercise[]) => {
+        this.availableExercises = exercises;
+        this.exercisesChanged.next([...this.availableExercises]);
+      });
   }
 
   startExercise(selectedId: string) {
