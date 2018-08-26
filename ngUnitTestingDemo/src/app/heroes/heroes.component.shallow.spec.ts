79075aebeff258e15b5ad4fed39361ffff51f267
Georgi Dimitrov <dgeorgi@vmware.com>
2018-08-26 22:18:42 +0100

    Shallow Integration Tests.

---
+++ ngUnitTestingDemo/src/app/heroes/heroes.component.shallow.spec.ts
@@ -0,0 +1,58 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA, Component, Input } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs/observable/of';
+
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+describe('HeroesComponent (shallow)', () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService;
+  let HEROES;
+
+  @Component({
+    selector: 'app-hero',
+    template: '<div></div>'
+  })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+    // @Output() delete = new EventEmitter();
+  }
+
+  beforeEach(() => {
+    HEROES = [
+      { id: 1, name: 'Spider Dude', strength: 8 },
+      { id: 2, name: 'Wonderful Dude', strength: 24 },
+      { id: 3, name: 'Super Dude', strength: 55 }
+    ];
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+    
+    TestBed.configureTestingModule({
+      declarations: [
+        HeroesComponent,
+        FakeHeroComponent
+      ],
+      providers: [
+        { provide: HeroService, useValue: mockHeroService }
+      ],
+      // schemas: [NO_ERRORS_SCHEMA]
+    });
+    fixture = TestBed.createComponent(HeroesComponent);
+  });
+
+  it('should set heroes correctly from the service', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    
+    expect(fixture.componentInstance.heroes.length).toBe(3);
+  });
+
+  it('should create one li for each hero', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    
+    expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
+  });
+});
