29ea8a7065f36a91223f6b3e5baf931f56199f74
Georgi Dimitrov <dgeorgi@vmware.com>
2018-08-27 12:14:20 +0100

    Deep integraton tests.

---
+++ ngUnitTestingDemo/src/app/hero.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed, ComponentFixture, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { By } from '@angular/platform-browser';
+
+import { HeroService } from './hero.service';
+import { MessageService } from './message.service';
+
+describe('HeroService', () => {
+  let mockMessageService;
+  let heroService: HeroService;
+  let httpTestingController: HttpTestingController;
+  
+  beforeEach(() => {
+    mockMessageService = jasmine.createSpyObj(['add']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        HeroService,
+        { provide: MessageService, useValue: mockMessageService }
+      ]
+    });
+
+    httpTestingController = TestBed.get(HttpTestingController);
+    heroService = TestBed.get(HeroService);
+  });
+
+  describe('getHero', () => {
+  it('should call get with the correct URL', () => {
+    heroService.getHero(4).subscribe();
+
+    const req = httpTestingController.expectOne('api/heroes/4');
+    req.flush({ id: 4, name: 'Super Dude', strength: 100 });
+    httpTestingController.verify();
+  });
+});
--- ngUnitTestingDemo/src/app/heroes/heroes.component.deep.spec.ts
+++ ngUnitTestingDemo/src/app/heroes/heroes.component.deep.spec.ts
@@ -5,21 +5,12 @@ import { of } from 'rxjs/observable/of';
 
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from '../hero.service';
-import { Hero } from '../hero';
+import { HeroComponent } from '../hero/hero.component';
 
-describe('HeroesComponent (shallow tests)', () => {
+describe('HeroesComponent (deep tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
   let mockHeroService;
   let HEROES;
-
-  @Component({
-    selector: 'app-hero',
-    template: '<div></div>'
-  })
-  class FakeHeroComponent {
-    @Input() hero: Hero;
-    // @Output() delete = new EventEmitter();
-  }
 
   beforeEach(() => {
     HEROES = [
@@ -32,27 +23,27 @@ describe('HeroesComponent (shallow tests)', () => {
     TestBed.configureTestingModule({
       declarations: [
         HeroesComponent,
-        FakeHeroComponent
+        HeroComponent
       ],
       providers: [
         { provide: HeroService, useValue: mockHeroService }
       ],
-      // schemas: [NO_ERRORS_SCHEMA]
+      schemas: [NO_ERRORS_SCHEMA]
     });
+
     fixture = TestBed.createComponent(HeroesComponent);
   });
+  
+  it('should render each hero as a HeroComponent', () => {
+    mockHeroService.getHeroes.and.returnValues(of(HEROES));
 
-  it('should set heroes correctly from the service', () => {
-    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    // runs ngOnInit
     fixture.detectChanges();
-    
-    expect(fixture.componentInstance.heroes.length).toBe(3);
-  });
 
-  it('should create one li for each hero', () => {
-    mockHeroService.getHeroes.and.returnValue(of(HEROES));
-    fixture.detectChanges();
-    
-    expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
+    const heroComponentDEs = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    expect(heroComponentDEs.length).toEqual(3);
+    for (let i = 0; i < heroComponentDEs.length; i++) {
+      expect(heroComponentDEs[i].componentInstance.hero).toEqual(HEROES[i]);
+    }
   });
 });
